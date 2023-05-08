e62220311c1ec0107843bae16fd3998bcaa38fad
radu <radu@radu.com>
2023-05-08 11:11:36 +0300

    New DSL Job

--- JenkinsDSLJob.groovy
+++ JenkinsDSLJob.groovy
@@ -1,6 +1,23 @@
-job('New Hello-World Job') {
-    description('New-Hello-World-Job')
-    steps {
-        shell('echo Hello World!')
+// Initial Deployment
+// job('New Hello-World Job') {
+//     description('New-Hello-World-Job')
+//     steps {
+//         shell('echo Hello World!')
+//     }
+// }
+
+job('Node-Basic-App') {
+    description('Node-Basic-Hello-World-Application')
+    scm {
+       git('https://github.com/rradu19/node-basic-app.git')
     }
+    
+    wrappers{
+        nodejs('nodejs')
+    }
+
+    steps {
+        shell('node app.js')
+    }
+
 }
